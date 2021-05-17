2cb49d88d14a38c0e017e2ac10e994078b9742a0
Oleg Shtanko <xiaklizrum@gmail.com>
2021-05-17 23:13:27 +1000

    WIP Unworked route with Atlas

--- app/routes.php
+++ app/routes.php
@@ -3,12 +3,24 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+use Atlas\Orm\Atlas;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
+    /** @var Atlas $atlas */
+    $atlas = $app->getContainer()->get('atlas');
+
+    $app->get('/users', function(Request $request, Response $response, array $args) use ($atlas){
+        $users = $atlas->select(/** Atlas Model here */)->fetchRecord();
+        return $response->withStatus(200)
+            ->withHeader('Content-Type', 'application/json')
+            ->getBody()
+            ->write(json_encode($users));
+    });
+
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
         return $response;
